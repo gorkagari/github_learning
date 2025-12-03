21341e88d1350af833131d50dc34f2fa956a1234
gorka <gorkagari@gmail.com>
2025-12-03 20:49:03 +0100

    Add github management tool

--- 01_repo_setup/python/repo_setup/repo_setup.py
+++ 01_repo_setup/python/repo_setup/repo_setup.py
@@ -1,7 +1,21 @@
+from github import Github
+from github import Auth
+
 import argparse
 import json
 
-def main(repository_list_json):
+def githubRepositoryList(github_token):
+
+    auth = Auth.Token(github_token)
+    g = Github(auth=auth)
+
+    for repo in g.get_user().get_repos():
+        print(repo.name)
+
+    # To close connections after use
+    g.close()
+
+def main(repository_list_json, github_token):
     print("Python script is starting...")
     print(f"Opening {repository_list_json}:")
 
@@ -15,12 +29,16 @@ def main(repository_list_json):
     for repo in repository_list:
         print(repo)
 
+    print("Currently available GitHub repsoitories:")
+    githubRepositoryList(github_token)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Get a JSON file with repository list"
     )
     parser.add_argument("--json_repository_list_path", required=True, type=str)
+    parser.add_argument("--github_token", required=True, type=str)
     args = parser.parse_args()
 
-    main(args.json_repository_list_path)
+    main(args.json_repository_list_path, args.github_token)
