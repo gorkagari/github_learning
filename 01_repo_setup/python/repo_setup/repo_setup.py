bdde7545dcb139d46100bec11d8620934f37c9c8
gorka <gorkagari@gmail.com>
2025-12-03 20:37:05 +0100

    Testing new argument parser

--- 01_repo_setup/python/repo_setup/repo_setup.py
+++ 01_repo_setup/python/repo_setup/repo_setup.py
@@ -1,9 +1,21 @@
-import sys
+import argparse
+import json
 
-def main():
+def main(repository_list_json):
     print("Python script is starting...")
-    print(sys.argv.append('--json_repository_list'))
+    print(f"Opening {repository_list_json}:")
+
+
+    with open(repository_list_json, "r") as f:
+        repository_list = json.load(f)
+    print(repository_list)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="Get a JSON file with repository list"
+    )
+    parser.add_argument("--json_repository_list_path", required=True, type=str)
+    args = parser.parse_args()
+
+    main(args.json_repository_list_path)
